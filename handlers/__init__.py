4fc41f0fc482647115c0fef3dc5b1cecc7fa8eb1
ssanssak-hub <ssanssak@gmail.com>
2025-10-24 02:57:54 +0330

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,24 +1,27 @@
 """
 🎮 handlers package برای ربات کنکور
 """
-from .main_menu import *
-from .countdown import *
-from .calendar import *
-from .reminders import *
-from .attendance import *
-from .study_plan import *
-from .statistics import *
-from .help import *
-from .admin import *
+
+from .main_menu import setup_main_menu_handlers
+from .countdown import setup_countdown_handlers
+from .calendar import setup_calendar_handlers
+from .reminders import setup_reminders_handlers
+from .messages import setup_messages_handlers
+from .attendance import setup_attendance_handlers
+from .study_plan import setup_study_plan_handlers
+from .statistics import setup_statistics_handlers
+from .help import setup_help_handlers
+from .admin import setup_admin_handlers
 
 __all__ = [
-    'setup_main_handlers',
-    'countdown_handler',
-    'calendar_handler', 
-    'reminder_handler',
-    'attendance_handler',
-    'study_plan_handler',
-    'statistics_handler',
-    'help_handler',
-    'admin_handler'
+    'setup_main_menu_handlers',
+    'setup_countdown_handlers',
+    'setup_calendar_handlers', 
+    'setup_reminders_handlers',
+    'setup_messages_handlers',
+    'setup_attendance_handlers',
+    'setup_study_plan_handlers',
+    'setup_statistics_handlers',
+    'setup_help_handlers',
+    'setup_admin_handlers'
 ]
